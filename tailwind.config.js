0e8a62890f8f7a4708fecd1c139db0602272a010
muhivan99 <134144781+muhivan99@users.noreply.github.com>
2025-09-18 19:07:41 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,5 @@ module.exports = {
   darkMode: 'class',
   content: ['./app/**/*.{js,jsx,ts,tsx,mdx}','./components/**/*.{js,jsx,ts,tsx,mdx}'],
   theme: { extend: {} },
-  plugins: []
+  plugins: [],
 };
