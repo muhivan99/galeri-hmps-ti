1769e8467b80076d7315c0d7843a749ac7ab7076
muhivan99 <134144781+muhivan99@users.noreply.github.com>
2025-09-18 18:48:02 +0700

    Update ThemeProvider.js

--- components/ThemeProvider.js
+++ components/ThemeProvider.js
@@ -3,7 +3,12 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 export default function ThemeProvider({ children }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"       // default GELAP
+      enableSystem={false}      // abaikan tema OS
+      disableTransitionOnChange // biar tidak kedip
+    >
       {children}
     </NextThemesProvider>
   );
