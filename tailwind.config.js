be886aa318c5630ddff566222f99faf4b50903a0
muhivan99 <134144781+muhivan99@users.noreply.github.com>
2025-09-18 18:57:03 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  // ⬇️ WAJIB: biar toggler bisa kasih class "dark" ke <html>
   darkMode: 'class',
-
-  content: [
-    './app/**/*.{js,jsx,ts,tsx,mdx}',
-    './components/**/*.{js,jsx,ts,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      // opsional — feel free to kosongin
-      container: { center: true, padding: '1rem' },
-    },
-  },
-  plugins: [],
+  content: ['./app/**/*.{js,jsx,ts,tsx,mdx}','./components/**/*.{js,jsx,ts,tsx,mdx}'],
+  theme: { extend: {} },
+  plugins: []
 };
